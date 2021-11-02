a8650ec580b2c98fdc4b4a6166f8852e617a223e
damien-verschaere <damien.verschaere@laplateforme.io>
2021-11-02 13:17:50 +0100

    job01 ok

--- jour1/job01/index.php
+++ jour1/job01/index.php
@@ -5,3 +5,23 @@
     // $global= $str2.''.$str.''.$str3;
 
   echo "{$str2} {$str} {$str3}";
+  echo '<br>';
+
+ $val = 6;
+ $addition=$val + 4;
+ echo $val;
+ echo '<br>';
+ echo $addition;
+ echo'<br>';  
+
+$mybool=true;
+ echo $mybool;
+ echo '<br>';
+ if($mybool=true){
+     $mybool=false;
+ }
+ var_dump ($mybool);
+
+
+
+
