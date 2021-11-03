f05df80ae3f0f2f19835038316bdf46eaa80ce68
damien-verschaere <damien.verschaere@laplateforme.io>
2021-11-03 12:45:07 +0100

    jour 2 job01

---
+++ jour2/job01/index.php
@@ -0,0 +1,15 @@
+<?php 
+    $compteur = 0;
+    while ($compteur<1337){
+         if ($compteur==42){
+        echo "<u>". "<b>".$compteur ."</u>" ."</b>" ."<br>";
+}
+    else{ echo $compteur ."<br>";
+    } 
+           $compteur ++;
+        }
+        
+    
+    ?>
+
+    
